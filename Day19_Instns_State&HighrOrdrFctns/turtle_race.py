2e1f1eb2cc75e9833671ad436f50916c6368d1cd
Citizen-XX <102715353+Citizen-XX@users.noreply.github.com>
2024-10-05 16:32:27 -0600

    'StartingDay20'

--- Day19_Instns_State&HighrOrdrFctns/turtle_race.py
+++ Day19_Instns_State&HighrOrdrFctns/turtle_race.py
@@ -1,16 +1,53 @@
 from turtle import Turtle, Screen
 
-screen = Screen()
-screen.setup(width=500,height=400)
-user_bet = screen.textinput(title='Make your bet',prompt='Which turtle will win the race? Enter a color: ')
-colors = ['red','orange','yellow','green','blue','purple']
-height_distribution = [i for i in range(-160,160,int(380/6))]
+import random
 
-for color,height in zip(colors,height_distribution):
-    tim = Turtle(shape='turtle')
-    tim.color(color)
-    tim.penup()
-    tim.speed(4)
-    tim.goto(x=-230,y=height)
+
+is_race_on = False
+
+screen = Screen()
+
+screen.setup(width=500, height=400)
+
+user_bet = screen.textinput(
+
+    title='Make your bet', prompt='Which turtle will win the race? Enter a color: ')
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
+height_distribution = [i for i in range(-160, 160, int(380/6))]
+
+all_turtles = []
+
+
+for color, height in zip(colors, height_distribution):
+    new_turtle = Turtle(shape='turtle')
+    new_turtle.color(color)
+    new_turtle.penup()
+    new_turtle.speed(4)
+    new_turtle.goto(x=-230, y=height)
+    all_turtles.append(new_turtle)
+
+
+if user_bet:
+
+    is_race_on = True
+
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(
+                    f"You've lost! The {winning_color} turtle is the winner!")
+        random_distance = random.randint(0, 10)
+
+        turtle.forward(random_distance)
+
 
 screen.exitonclick()
